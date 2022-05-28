e4c6287096d5f89713e11b2d134a47dd9f0d21e9
Iftee1711402 <tawsifhaque97@gmail.com>
2022-05-29 03:24:01 +0600

    refactor

--- app.js
+++ app.js
@@ -5,35 +5,34 @@ const ejs = require("ejs");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 
-// console.log(process.env.DB_URL);
-
+// mongodb cloud atlas 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/blog_crud";
 
 // connecting to mongoose
 async function main() {
-  await mongoose.connect(dbUrl);
-  console.log("database connected");
+    await mongoose.connect(dbUrl);
+    console.log("database connected");
 }
 
 main().catch((err) => {
-  console.log("mongo connection error:", err);
+    console.log("mongo connection error:", err);
 });
 
 // schema
 const postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
+    title: String,
+    content: String,
 });
 
 // model
 const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+    "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+    "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -45,62 +44,62 @@ app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 app.get("/", async function (req, res) {
-  const posts = await Post.find({});
-  res.render("home", { homeStartingContent, posts });
+    const posts = await Post.find({});
+    res.render("home", { homeStartingContent, posts });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { aboutContent });
+    res.render("about", { aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent });
+    res.render("contact", { contactContent });
 });
 
 app.get("/compose", function (req, res) {
-  res.render("compose");
+    res.render("compose");
 });
 
 app.post("/compose", async function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-  const newPost = await Post(post);
-  newPost.save();
-  res.redirect("/");
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postBody,
+    };
+    const newPost = await Post(post);
+    newPost.save();
+    res.redirect("/");
 });
 
 app.get("/posts/:id", async function (req, res) {
-  const { id } = req.params;
-  const post = await Post.findById(id);
-  res.render("post", { post });
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.render("post", { post });
 });
 
 app.get("/posts/:id/edit", async function (req, res) {
-  const { id } = req.params;
-  const post = await Post.findById(id);
-  res.render("edit", { post });
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.render("edit", { post });
 });
 
 app.put("/posts/:id", async function (req, res) {
-  const { id } = req.params;
-  const updatedPost = await Post.findByIdAndUpdate(id, {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  });
-  res.redirect(`/posts/${id}`);
+    const { id } = req.params;
+    const updatedPost = await Post.findByIdAndUpdate(id, {
+        title: req.body.postTitle,
+        content: req.body.postBody,
+    });
+    res.redirect(`/posts/${id}`);
 });
 
 app.delete("/posts/:id", async function (req, res) {
-  const { id } = req.params;
-  const deletedPost = await Post.findByIdAndRemove(id);
-  res.redirect("/");
+    const { id } = req.params;
+    const deletedPost = await Post.findByIdAndRemove(id);
+    res.redirect("/");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+    console.log(`server running on port ${port}`);
 });
 
 // tawsifhaque-daily-journal
